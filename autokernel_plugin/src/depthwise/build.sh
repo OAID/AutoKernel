4cc3a8b5ed79488dc77f5f1f0a2f9341c37e2a7d
QingChuanWS <bingshan45@163.com>
2020-12-11 09:34:49 +0000

    sync the lastest version of autokernel

--- autokernel_plugin/src/depthwise/build.sh
+++ autokernel_plugin/src/depthwise/build.sh
@@ -1,7 +1,7 @@
 g++ depthwise_gen.cc ../../common/GenGen.cpp \
-	-I /workspace/Halide/halide-build/include/ \
-	-L /workspace/Halide/halide-build/src \
-	-lHalide -std=c++11 -fno-rtti \
+	-I ${HALIDE_DIR}/include \
+	-L ${HALIDE_DIR}/lib \
+        -lHalide -std=c++11 -fno-rtti \
 	-o depthwise_gen
 
-./depthwise_gen -g halide_depthwise -e c_header,assembly -o . target=host
+./depthwise_gen -g halide_depthwise -e c_header,assembly -o . target=host-no_runtime-no_asserts-no_bounds_query
